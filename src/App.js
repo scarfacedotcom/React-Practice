bd6aac5f4d65f451f8441086eec1c1289855e525
scarfacedotcom <peteradaaku@gmail.com>
2022-07-19 14:27:29 +0100

    few changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import { useState } from 'react'
 
 function App() {
   const [ name, setName] = useState('peter')
+  const [events, setEvents] = useState([
+    {title: "mario's birthday bash", id: 1},
+    {title: "bowser's live stream", id: 2},
+    {title: "race on moo moo farm", id: 3}
+  ])
 
   const handleClick = () => {
     setName('scarface')
@@ -12,6 +17,11 @@ function App() {
   return (
     <div className="App">
       <h2>My name is {name} </h2>
+      {events.map((event) => [
+        <ul key={event.id}>
+          <li>{event.title}</li>
+        </ul>
+      ])}
       <button onClick={handleClick}>Change Name</button>
     </div>
   );
