706bcd7566960d8250aaeb542990c3b7ae9d127f
scarfacedotcom <peteradaaku@gmail.com>
2022-07-21 08:39:22 +0100

    few changes

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -11,19 +11,47 @@ export default function NewEventForm() {
     console.log(e.target.value)
   }
 
+  const handleReset = () => {
+    setDate('');
+    setTitle('');
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const event = {
+      title : title,
+      date: date,
+      id: Math.random(Math.floor *1000)
+    }
+    handleReset();
+    console.log(event)
+  }
+
   return (
-    <form className="new-event-form">
+    <form className="new-event-form" onSubmit={handleSubmit}>
       <label>
         <span>Event Title:</span>
-        <input type="text" onChange={() => setTitle(e.target.value)} />
+        <input 
+          type="text" 
+          onChange={(e) => setTitle(e.target.value)} 
+          value={title}
+        />
       </label>
 
       <label>
         <span>Event Date</span>
-        <input type="date" onChange={() => setDate(e.target.value)} />
+        <input 
+          type="date" 
+          onChange={(e) => setDate(e.target.value)} 
+          value={date}
+        />
       </label>
 
       <button>Submit</button>
+      <button onClick={handleReset}>Reset</button>
+
+      <p>Title- {title} , Date- {date} </p>
     </form>
   )
 }
